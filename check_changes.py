7e9ba78e47f2bab7eb45f4329adf6e50a4e2bfc5
witold <wiltold.wroblewski@sennder.com>
2021-01-05 19:20:05 +0100

    add netlify build hook logic

--- check_changes.py
+++ check_changes.py
@@ -1,8 +1,13 @@
 import argparse
+import os
+import requests
 from datetime import datetime
+from dotenv import load_dotenv
 from logzero import logger
 
 from sizun.db import count_new_advertisements
+
+load_dotenv()
 
 
 def main(args):
@@ -14,7 +19,13 @@ def main(args):
         )
         count = count_new_advertisements(start_date)
         if count:
-            logger.info(f'{count} new houses saved.')
+            logger.info(f'{count} new houses saved. Triggering new build.')
+            netlify_hook = os.getenv('NETLIFY_BUILD_HOOK')
+            if not netlify_hook:
+                raise ValueError('NETLIFY_BUILD_HOOK variable should be set.')
+            r = requests.post(netlify_hook)
+            r.raise_for_status()
+            logger.info('Build triggered.')
     except ValueError as error:
         logger.error('Date cannot be converted to format YYYY-mm-ddTHH:MM:SS')
         raise error
