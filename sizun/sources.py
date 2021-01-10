9f9e2f097916d0ceb689f21cf6e93331f0edc5ec
witold <wiltold.wroblewski@sennder.com>
2021-01-10 18:47:03 +0100

    add new source for ouest france

--- sizun/sources.py
+++ sizun/sources.py
@@ -1,5 +1,7 @@
 OUEST_FRANCE_SOURCES = [
-    'https://www.ouestfrance-immo.com/acheter/?types=maison,demeure-exception,longere-et-corps-de-ferme,terrain&lieux=13157,13137,12946,13083,200218,12941,12994,13165,13006&tri=DATE_DECROISSANT&prix=0_150000'
+    'https://www.ouestfrance-immo.com/acheter/?types=maison,demeure-exception,longere-et-corps-de-ferme,terrain&lieux=13157,13137,12946,13083,200218,12941,12994,13165,13006&tri=DATE_DECROISSANT&prix=0_150000',
+    'https://www.ouestfrance-immo.com/acheter/?types=maison,demeure-exception,longere-et-corps-de-ferme,terrain&lieux=12965,13107,13167,13026&tri=DATE_DECROISSANT&prix=0_150000',
+    'https://www.ouestfrance-immo.com/acheter/douarnenez-29-29100/?types=maison,demeure-exception,longere-et-corps-de-ferme,terrain&tri=DATE_DECROISSANT&prix=0_150000'
 ]
 
 IMMNONOT_SOURCES = [
