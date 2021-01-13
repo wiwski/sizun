b93306e9739bf88e3ac22f76b61af3b2b112f81a
witold <wiltold.wroblewski@sennder.com>
2021-01-13 14:10:05 +0100

    add first time source inserted check (with modification of created field)

--- sizun/db.py
+++ sizun/db.py
@@ -1,10 +1,11 @@
 import os
 import pathlib
 from datetime import datetime
+from typing import List, Optional
 
 from dotenv import load_dotenv
 from logzero import logger
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker
 
 from .models.advertisement import Advertisement
@@ -17,14 +18,24 @@ engine = create_engine(f'sqlite:///{sql_lite_db_path}')
 Advertisement.metadata.create_all(engine)
 
 
-def save_advertisements(advertisements: Advertisement):
+def save_advertisements(advertisements: Advertisement, change_if_first_time=True):
     session = (sessionmaker(bind=engine))()
+
+    # Checks if it is first time we insert the source in the DB.
+    # If it is, we modify the created field so that it doesn't appears in front of the latests results
+    all_sources_in_db = _get_all_source_in_db()
+
     inserted_advertisements_count = 0
     for ad in advertisements:
+        # Checks another ad with same URL doesn't already exist
         match = session.query(Advertisement).filter_by(
             url=ad.url
         ).count()
         if not match:
+            is_first_source_insert = ad.source not in all_sources_in_db
+            if is_first_source_insert:
+                oldest_advertisement_created_date = _get_oldest_advertisement_creation_date()
+                ad.created = oldest_advertisement_created_date
             session.add(ad)
             inserted_advertisements_count += 1
     session.commit()
@@ -48,3 +59,20 @@ def delete_advertisements_with_params(**kwargs):
     rows_deleted = session.query(Advertisement).filter_by(**kwargs).delete()
     session.commit()
     return rows_deleted
+
+
+def _get_all_source_in_db() -> List[str]:
+    import itertools
+    session = (sessionmaker(bind=engine))()
+    query = session.query(Advertisement.source).group_by(
+        Advertisement.source).all()
+    session.close()
+    return itertools.chain(*query)
+
+
+def _get_oldest_advertisement_creation_date() -> Optional[datetime]:
+    session = (sessionmaker(bind=engine))()
+    oldest_advertisement_created_date = session.query(
+        Advertisement.created).order_by(Advertisement.created.asc()).first()
+    if oldest_advertisement_created_date:
+        return oldest_advertisement_created_date[0]
